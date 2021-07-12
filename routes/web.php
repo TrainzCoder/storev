af23d4a633e685ba238cd024d60f3d79ac49350e
Kuuhaku_NGNL <53076870+Sylent-Sys@users.noreply.github.com>
2021-07-12 17:42:08 +0800

    Revert "Create login register simple home and reset page"

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/home', 'home')->name('home');
-Route::view('/login', 'login')->name('login');
-Route::view('/register', 'register')->name('register');
-Route::view('/reset', 'reset')->name('reset');
+Route::view('/', 'welcome')->name('home');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                rubik: ['Rubik', 'sans-serif']
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
-            colors: {
-                "darkBlue": "#01438a",
-                "crayola": "#00a5cf",
-                "aquamarine": "#9fffcb",
-                "persianGreen": "#25a18e",
-                "lightGreen": "#7ae582"
-            },
-            backgroundImage: {
-                'shopBanner': "url('/img/banner/shop-banner.jpg')",
-                'secureBanner': "url('/img/banner/secure-banner.jpg')",
-                'saleBanner': "url('/img/banner/sale-banner.jpg')"
-            }
         },
     },
     variants: {
